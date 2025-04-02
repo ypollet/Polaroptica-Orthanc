06b4ee6d13c9e88ed3f92e0c7f83f1e1ecfd0477
Yann Pollet <pollet.yann1999@gmail.com>
2025-04-02 11:41:53 +0200

    Fix : update frontend

--- DicomizePolaropticaFiles.py
+++ DicomizePolaropticaFiles.py
@@ -12,7 +12,7 @@
 
 # the License, or (at your option) any later version.
 
-# 
+#
 
 # This program is distributed in the hope that it will be useful, but
 
@@ -38,26 +38,26 @@ import json
 import requests
 import os
 
-orthanc_server = "http://localhost:8042" #"https://naturalheritage.africamuseum.be/Orthanc"
+orthanc_server = "http://localhost:8042"
 
 path_to_project = "data/A09135/isrnbel001_r1_xpl_rotated"
-SOURCE = f'{path_to_project}/*.jpg'
-calib_file = f'{path_to_project}/rotation.json'
+SOURCE = f"{path_to_project}/*.jpg"
+calib_file = f"{path_to_project}/rotation.json"
 images = sorted(glob.glob(SOURCE))
 i = 0
 
 
-with open(calib_file, 'rb') as f:
+with open(calib_file, "rb") as f:
     rotation_dict = json.load(f)
-    
+
 study_uid = pydicom.uid.generate_uid()
 series_uid = pydicom.uid.generate_uid()
 
-if "Thumbnails" in rotation_dict and rotation_dict["Thumbnails"]:
-    thumbnails_width = rotation_dict["Thumbnails_width"]
-    thumbnails_height  = rotation_dict["Thumbnails_height"]
+if "thumbnails" in rotation_dict and rotation_dict["thumbnails"]:
+    thumbnails_width = rotation_dict["thumbnails_width"]
+    thumbnails_height = rotation_dict["thumbnails_height"]
 
-images = rotation_dict["Rotation"]
+images = rotation_dict["rotation"]
 
 now = datetime.datetime.now()
 
@@ -67,50 +67,48 @@ for image_name in images:
     camera = os.path.basename(image)
     print(camera)
     ds = pydicom.dataset.Dataset()
-    ds.PatientName = 'Lame Meteorite^isrnbel001'
-    ds.PatientID = 'isrnbel001'
-    ds.PatientBirthDate = '20200914'
-    ds.PatientSex = 'O'
+    ds.PatientName = "Lame Meteorite^isrnbel001"
+    ds.PatientID = "isrnbel001"
+    ds.PatientBirthDate = "20200914"
+    ds.PatientSex = "O"
 
-    
-    ds.StudyDate = now.strftime('%Y%m%d')
-    ds.StudyTime = now.strftime('%H%M%S')
+    ds.StudyDate = now.strftime("%Y%m%d")
+    ds.StudyTime = now.strftime("%H%M%S")
 
-    ds.ImageType = [ 'ORIGINAL', 'PRIMARY' ]
-    ds.UserContentLabel = camera
-    ds.Laterality = 'L'
-    ds.LossyImageCompression = '01'
-    ds.Modality = 'XC'  # External-camera photography
+    ds.ImageType = ["ORIGINAL", "PRIMARY"]
+    ds.UserContentLabel = "_".join(camera.split(".")[0].split("_")[1:3])
+    ds.Laterality = "L"
+    ds.LossyImageCompression = "01"
+    ds.Modality = "XC"  # External-camera photography
     ds.SOPClassUID = pydicom.uid.VLPhotographicImageStorage
     ds.SOPInstanceUID = pydicom.uid.generate_uid()
     ds.SeriesInstanceUID = series_uid
     ds.StudyInstanceUID = study_uid
     ds.PixelSpacing = rotation_dict["PixelRatio"]
-    ds.RotationAngle = image_data["Angle"]
-    
+    ds.RotationAngle = image_data["angle"]
+
     ds.AccessionNumber = None
     ds.ReferringPhysicianName = None
     ds.SeriesNumber = None
     ds.StudyID = None
-    ds.StudyDescription = "Lame meteorite polarisee"
+    ds.StudyDescription = "Lame polarisee"
     ds.SeriesDescription = "R1 XPL"
     ds.Manufacturer = None
     ds.AcquisitionContextSequence = None
-    ds.InstanceNumber = i+1
-
+    ds.InstanceNumber = i + 1
 
     # Basic encapsulation of color JPEG
     # httpss://pydicom.github.io/pydicom/stable/tutorials/pixel_data/compressing.html
 
-    with open(image, 'rb') as f:
-        frames = [ f.read() ]
+    with open(image, "rb") as f:
+        frames = [f.read()]
         ds.PixelData = pydicom.encaps.encapsulate(frames)
-    
+
     with PIL.Image.open(image) as im:
         ds.Rows = im.size[1]
         ds.Columns = im.size[0]
         thumbnail_buffer = None
-        if "Thumbnails" in rotation_dict and rotation_dict["Thumbnails"]:
+        if "thumbnails" in rotation_dict and rotation_dict["thumbnails"]:
             im.thumbnail((thumbnails_width, thumbnails_height))
             thumbnail_buffer = BytesIO()
             im.save(thumbnail_buffer, format="JPEG")
@@ -121,28 +119,29 @@ for image_name in images:
     ds.BitsStored = 8
     ds.HighBit = 7
     ds.PixelRepresentation = 0
-    ds.PhotometricInterpretation = 'YBR_FULL_422'
+    ds.PhotometricInterpretation = "YBR_FULL_422"
 
-    ds['PixelData'].VR = 'OB'  # always for encapsulated pixel data
+    ds["PixelData"].VR = "OB"  # always for encapsulated pixel data
     ds.is_little_endian = True
     ds.is_implicit_VR = False
 
     meta = pydicom.dataset.FileMetaDataset()
     meta.TransferSyntaxUID = pydicom.uid.JPEGBaseline8Bit
     ds.file_meta = meta
-        
-    out : BytesIO = BytesIO()
+
+    out: BytesIO = BytesIO()
     ds.save_as(out, write_like_original=False)
-    
-    response = requests.post(f'{orthanc_server}/instances', out.getvalue())
-    
+
+    response = requests.post(f"{orthanc_server}/instances", out.getvalue())
+
     response.raise_for_status()
-    
+
     uuid = response.json()["ID"]
     series_uuid = response.json()["ParentSeries"]
-    
-    if(thumbnail_buffer):
-        r = requests.put(f'{orthanc_server}/instances/{uuid}/attachments/thumbnail', data=thumbnail_buffer.getvalue())
+
+    if thumbnail_buffer:
+        r = requests.put(
+            f"{orthanc_server}/instances/{uuid}/attachments/thumbnail",
+            data=thumbnail_buffer.getvalue(),
+        )
     i += 1
-    
-    
